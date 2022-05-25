b84ff64c27576e148955f3737256801cfc002091
Willian da silva soares <90284411+willianglitch@users.noreply.github.com>
2022-05-25 10:17:44 -0300

    commit 8
    tava cometendo um erro , não tava construindo todo o algoritmo antes de começar a coda em javascript.

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+/*
+ ==================                      BABY STEPS                         ==============
+objetivo 1 - preciso que o icone mude quando o mouse passe em cima de outro personagem .
+temos que :
+--colocar a classe " selecionado " em cima do personagem em que o mouse passou 
+em cima para adicionar a animação.
+
+objetivo 2 - retirar a animação de selecionado do pikachu. 
+--preciso retirar a classe selecionado do personagem em que o mouse não está mais
+em cima .
+
+objetico 3 - quando o mouse passar em cima ,troca a imagem do personagem e nome.
+temos que :
+-- alterar a imagem personagem jogador 01 
+-- alterar o nome personagem jogador 01
+*/
+const lucario = document.getElementById('lucario')
+console.log(lucario)
